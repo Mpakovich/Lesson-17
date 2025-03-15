40590a87b4c320bd92348265e870bc9f93d9b2dc
Mpakovich <56260995f@gmail.com>
2025-03-15 11:48:14 +0200

    prettier

--- tests/pages/order-page.ts
+++ tests/pages/order-page.ts
@@ -1,12 +1,19 @@
-import { Locator, Page } from '@playwright/test'
+import { Page } from '@playwright/test';
+import { Input } from '../atoms/Input';
+import { Button } from '../atoms/Button';
 
 export class OrderPage {
-  readonly page: Page
-  readonly statusButton: Locator
-  // add more locators here
+  readonly page: Page;
+  readonly statusButton: Button;
+  readonly nameField: Input;
+  readonly phoneField: Input;
+  readonly commentField: Input;
 
   constructor(page: Page) {
-    this.page = page
-    this.statusButton = page.getByTestId('openStatusPopup-button')
+    this.page = page;
+    this.statusButton = new Button(page, '[data-name="openStatusPopup-button"]');
+    this.nameField = new Input(page, '#name');
+    this.phoneField = new Input(page, '#phone');
+    this.commentField = new Input(page, '#comment');
   }
 }
